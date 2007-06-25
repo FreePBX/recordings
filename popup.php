37c8cd4e3d87acbd2d059f246b49307c131b33ce
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-06-25 16:03:03 +0000

    change title of popup to FreePBX from ARI - where this was taken from

--- popup.php
+++ popup.php
@@ -12,12 +12,12 @@ include_once("crypt.php");
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <TITLE>ARI</TITLE>
-    <link rel="stylesheet" href="popup.css" type="text/css">
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  </head>
-  <body>
+	<head>
+		<TITLE>FreePBX Recording Review</TITLE>
+		<link rel="stylesheet" href="popup.css" type="text/css">
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	</head>
+	<body>
 
 <?php
 
