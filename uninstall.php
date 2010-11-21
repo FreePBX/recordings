9651d49c10a3e0877b0672ac0eb617e1c63db687
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-11-21 19:46:33 +0000

    closes #4651 update module uninstalls to ignore feature codes now being deleted by module_uninstall

--- uninstall.php
+++ uninstall.php
@@ -2,22 +2,9 @@
 
 // Delete them all even if they should not exist just in case
 //
-$recordings = recordings_list();
-foreach ($recordings as $item) {
-	$fcc = new featurecode('recordings', 'edit-recording-'.$item['id']);
-	$fcc->delete();
-	unset($fcc);	
-}
+
+// Don't bother uninstalling feature codes, now module_uninstall does it
 
 sql('DROP TABLE IF EXISTS recordings');
-
-$fcc = new featurecode('recordings', 'record_save');
-$fcc->delete();
-unset($fcc);
-
-$fcc = new featurecode('recordings', 'record_check');
-$fcc->delete();
-unset($fcc);
-
 
 ?>
