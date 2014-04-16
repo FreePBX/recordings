b0bde3d4ab6319d80e3600243635376bf32959a1
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 16:52:33 -0500

    new license tags

--- page.recordings.php
+++ page.recordings.php
@@ -1,19 +1,6 @@
 <?php 
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 /* $Id$ */
-//Copyright (C) 2004 Coalescent Systems Inc. (info@coalescentsystems.ca)
-//
-//Re-written by Rob Thomas <xrobau@gmail.com> 20060318.
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2
-//of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
 
 $action = isset($_REQUEST['action'])?$_REQUEST['action']:'';
 
