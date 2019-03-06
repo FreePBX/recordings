c4399620581d585eec54505882fca7850d2add63
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 17:51:05 -0500

    [Module Tag script: recordings 15.0.3.2]

--- Backup.php
+++ Backup.php
@@ -2,27 +2,28 @@
 namespace FreePBX\modules\Recordings;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-	$configs = $this->FreePBX->Recordings->getAll();
-	$files = [];
-	$dirs = [];
-	$base = $this->FreePBX->Config->get('ASTVARLIBDIR');
-	foreach($configs as $config){
-		foreach($config['files'] as $file){
-			foreach($file as $key => $value){
-				$path = $base.'/sounds/'.$key.'/custom';
-				$dirs[$path] = $path;
-				foreach($value as $recordingfile){
-					if(!file_exists($recordingfile)){
-						continue;
+	public function runBackup($id,$transaction){
+		$dirs = [];
+		$base = $this->FreePBX->Config->get('ASTVARLIBDIR');
+		foreach($configs as $config){
+			foreach($config['files'] as $file){
+				foreach($file as $key => $value){
+					$path = $base.'/sounds/'.$key.'/custom';
+					$dirs[$path] = $path;
+					foreach($value as $recordingfile){
+						if(!file_exists($recordingfile)){
+							continue;
+						}
+						$this->addFile(basename($recordingfile),$path,'ASTVARLIBDIR',"recording");
 					}
-					$this->addFile(basename($recordingfile),$path,'ASTVARLIBDIR',"recording");
 				}
 			}
 		}
+		$this->addDirectories($dirs);
+		$this->addDependency('soundlang');
+		$this->addConfigs([
+			'data' => $this->FreePBX->Recordings->getAll(),
+			'features' => $this->dumpFeatureCodes()
+		]);
 	}
-	$this->addDirectories($dirs);
-	$this->addDependency('Soundlang');
-	$this->addConfigs($configs);
-  }
 }
--- Restore.php
+++ Restore.php
@@ -6,15 +6,15 @@ class Restore Extends Base\RestoreBase{
 		$configs = $this->getConfigs();
 		$files = $this->getFiles();
 		foreach($files as $file){
-				if($file['type'] == 'recording'){
-					$filename = $file['pathto'].'/'.$file['filename'];
-					$filename = $this->nameTest($filename,$file['base']);
-					if(file_exists($filename)){
-						copy($this->tmpdir.'/files/'.$file['pathto'].'/'.$file['filename'],$filename);
-					}
+			if($file['type'] == 'recording'){
+				$filename = $file['pathto'].'/'.$file['filename'];
+				$filename = $this->nameTest($filename,$file['base']);
+				if(file_exists($filename)){
+					copy($this->tmpdir.'/files/'.$file['pathto'].'/'.$file['filename'],$filename);
 				}
+			}
 		}
-		foreach($configs as $config) {
+		foreach($configs['data'] as $config) {
 			$recording = $this->FreePBX->Recordings->getRecordingById($config['id']);
 			$files = array_keys($config['files']);
 			$files = implode($files,'&');
@@ -25,6 +25,7 @@ class Restore Extends Base\RestoreBase{
 				$this->FreePBX->Recordings->updateRecording($config['id'],$config['displayname'],$config['description'],$files,$config['fcode'],$config['fcode_pass']);
 			}
 		}
+		$this->importFeatureCodes($config['features']);
 	}
 	public function nameTest($path,$var){
 		$sysPath = $this->FreePBX->Config->get($var);
@@ -36,5 +37,10 @@ class Restore Extends Base\RestoreBase{
 		$i = array_search('sounds',$pathArr);
 		$pathArr = array_slice($pathArr,$i);
 		return $sysPath.'/'.implode('/',$pathArr).'/'.$file;
+	}
+
+	public function processLegacy($pdo, $data, $tables, $unknownTables) {
+		$this->restoreLegacyDatabase($pdo);
+		$this->restoreLegacyFeatureCodes($pdo);
 	}
 }
