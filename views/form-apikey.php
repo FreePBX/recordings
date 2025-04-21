813bc6909546b87e7d52d4f07c43e1570abeab73
Franck <24569618+danardf@users.noreply.github.com>
2025-04-21 12:56:46 +0200

    Fixe ajax error
    Fixe ajax error when Open AI Apy key is empty.

--- views/form-apikey.php
+++ views/form-apikey.php
@@ -14,7 +14,7 @@
 						<label class="control-label"><?= _("Enter your API Key")?></label>
 					</div>
 					<div class="col-md-9">
-                        <input type="text" id="apikey" class="form-control" placeholder="<?= _("Enter your API Key here.") ?>" value="<?= $apikey ?>">
+                        <input type="text" id="apikey" class="form-control" placeholder="<?= _("Enter your API Key here.") ?>" value="<?= empty($apikey)? "" : $apikey; ?>">
 					</div>
 				</div>
 			</div>
